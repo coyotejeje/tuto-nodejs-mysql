6612a7a5c31e7d338d7dea04ac93a0faf7f730fc
jerome <micl.jerome@gmail.com>
2022-08-24 15:37:01 +0200

    created first API Rest with Node JS

---
+++ src/routes/createPokemon.js
@@ -0,0 +1,23 @@
+const { Pokemon } = require('../db/sequelize')
+const { ValidationError, UniqueConstraintError } = require('sequelize')
+const auth = require('../auth/auth')
+
+module.exports = (app) => {
+  app.post('/api/pokemons', auth, (req, res) => {
+    Pokemon.create(req.body)
+      .then(pokemon => {
+        const message = `Le pokémon ${req.body.name} a bien été crée.`
+        res.json({ message, data: pokemon })
+      })
+      .catch(error => {
+        if(error instanceof ValidationError) {
+          res.status(400).json({ message: error.message, data: error })
+        }
+        if(error instanceof UniqueConstraintError) {
+          res.status(400).json({ message: error.message, data: error })
+        }
+        const message = "Le pokémon n'a pas pu être ajoutée. Réessayez dans quelques instants."
+        res.status(500).json({ message, data: error })
+      })
+  })
+}
